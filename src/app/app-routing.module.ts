1414977bcd988730f3c27574f383f89cc0722427
Eduardo-Vendrami <86382508+Eduardo-Vendrami@users.noreply.github.com>
2023-02-28 01:26:00 -0300

    fisrt commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { JobsPageComponent } from './pages/jobs-page/jobs-page.component';
+import { FramePageComponent } from './pages/master/frame.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: FramePageComponent,
+    children: [
+      { path: '', component: HomePageComponent },
+      { path: 'jobs', component: JobsPageComponent },
+      { path: 'contact', component: ContactPageComponent }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { JobsPageComponent } from './pages/jobs-page/jobs-page.component';
+import { FramePageComponent } from './pages/master/frame.page';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    JobsPageComponent,
+    ContactPageComponent,
+    FramePageComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
